6bd709d37ad5a0669fcc390bd28f51c0ea8a467e
StephFlooo <stephany.flores15893@est.uca.edu.ni>
2023-04-28 08:19:55 -0600

    para sistematico

---
+++ principal.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+#include "funcion1.cpp"
+#include "funcion2.cpp"
+#include "funcion3.cpp"
+
+int main() {
+    funcion1();
+    funcion2();
+    funcion3();
+   
+    return 0;
+}
